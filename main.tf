d82f20ad858cdc9ebce6776fd55404e94d761209
purplefeetguy <purplefeetguy@hotmail.com>
2019-05-13 06:31:03 -0500

    added count variable to allow for the creation of multiple VMs

--- main.tf
+++ main.tf
@@ -12,6 +12,8 @@ variable "web_server_address_prefix" {}
 variable "web_server_name" {}
 
 variable "environment" {}
+variable "web_server_count" {}
+
 
 
 provider "azurerm" {
@@ -43,31 +45,33 @@ resource "azurerm_virtual_network" "web_server_vnet" {
 }
 
 resource "azurerm_subnet" "web_server_subnet" {
-  name                 = "${var.resource_prefix}-subnet"
-  resource_group_name  = "${azurerm_resource_group.web_server_rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.web_server_vnet.name}"
-  address_prefix       = "${var.web_server_address_prefix}"
+  name                      = "${var.resource_prefix}-subnet"
+  resource_group_name       = "${azurerm_resource_group.web_server_rg.name}"
+  virtual_network_name      = "${azurerm_virtual_network.web_server_vnet.name}"
+  address_prefix            = "${var.web_server_address_prefix}"
+  network_security_group_id = "${azurerm_network_security_group.web_server_nsg.id}"
 }
 
 resource "azurerm_network_interface" "web_server_nic" {
-  name                = "${var.web_server_name}-nic"
+  name                = "${var.web_server_name}-nic-${format("%02d",count.index)}"
   location            = "${var.web_server_location}"
   resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
-  network_security_group_id = "${azurerm_network_security_group.web_server_nsg.id}"
+  count               = "${var.web_server_count}"
 
   ip_configuration {
-      name                          = "${var.web_server_name}-ip"
+      name                          = "${var.web_server_name}-ip-${format("%02d",count.index)}"
       subnet_id                     = "${azurerm_subnet.web_server_subnet.id}"
       private_ip_address_allocation = "dynamic"
-      public_ip_address_id          = "${azurerm_public_ip.web_server_public_ip.id}"
+      public_ip_address_id          = "${azurerm_public_ip.web_server_public_ip.*.id[count.index]}"
   }
 }
 
 resource "azurerm_public_ip" "web_server_public_ip" {
-    name                         = "${var.web_server_name}-public-ip"
+    name                         = "${var.web_server_name}-public-ip-${format("%02d",count.index)}"
     location                     = "${var.web_server_location}"
     resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
     allocation_method            = "${var.environment == "production" ? "Static" : "Dynamic"}"
+    count                        = "${var.web_server_count}"
 }
 
 resource "azurerm_network_security_group" "web_server_nsg" {
@@ -89,3 +93,71 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
     resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
     network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
 }
+
+resource "azurerm_network_security_rule" "web_server_nsg_rule_ssh" {
+    name                        = "SSH Inbound"
+    priority                    = 101
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "TCP"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
+    network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
+}
+
+resource "azurerm_virtual_machine" "web_server" {
+    name                         = "${var.web_server_name}-${format("%02d",count.index)}"
+    location                     = "${var.web_server_location}"
+    resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
+    network_interface_ids        = ["${azurerm_network_interface.web_server_nic.*.id[count.index]}"]
+    vm_size                      = "Standard_B1s"
+    availability_set_id          = "${azurerm_availability_set.web_server_availability_set.id}"
+    count                        = "${var.web_server_count}"
+    
+    # storage_image_reference {
+    #     publisher = "MicrosoftWndowsServer"
+    #     offer     = "WindowsServer"
+    #     sku       = "2016-Datacenter-Server-Core-smalldisk"
+    #     version   = "2016.127.20190416"
+    # }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+    storage_os_disk {
+        name              = "${var.web_server_name}-os-${format("%02d",count.index)}"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    os_profile {
+        computer_name  = "${var.web_server_name}-${format("%02d",count.index)}"
+        admin_username = "webserver"
+        admin_password = "Passw0rd123!"
+    }
+
+    # os_profile_windows_config {
+
+    # }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+
+}
+
+resource "azurerm_availability_set" "web_server_availability_set" {
+      name                        = "${var.resource_prefix}-availability-set"
+      location                    = "${var.web_server_location}"
+      resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
+      managed                     = true
+      platform_fault_domain_count = 2
+}
+
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,5 +3,6 @@
 "resource_prefix"           = "web-server"
 "web_server_address_space"  = "1.0.0.0/22"
 "web_server_address_prefix" = "1.0.1.0/24"
-"web_server_name"           = "web-01"
+"web_server_name"           = "web"
 "environment"               = "production"
+"web_server_count"          = 2
