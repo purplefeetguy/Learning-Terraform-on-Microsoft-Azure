97aacfc22504e8fbec5eacb9acadbad03659f062
u770572 <jose.castillo@footlocker.com>
2019-05-14 05:23:19 -0500

    added count variable to nsg rules to determine to build only if production

--- main.tf
+++ main.tf
@@ -93,6 +93,7 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
     destination_address_prefix  = "*"
     resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
     network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
+    count                       = "${var.environment == "production" ? 1 : 0 }"
 }
 
 resource "azurerm_network_security_rule" "web_server_nsg_rule_ssh" {
@@ -107,6 +108,7 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_ssh" {
     destination_address_prefix  = "*"
     resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
     network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
+    count                       = "${var.environment == "production" ? 1 : 0 }"
 }
 
 resource "azurerm_virtual_machine" "web_server" {
