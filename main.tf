0dd96c9ac44b7e99481b4e58f9f4c41a716f44bb
u770572 <jose.castillo@footlocker.com>
2019-05-14 09:10:17 -0500

    added load-balancer to scale set

--- main.tf
+++ main.tf
@@ -14,6 +14,9 @@ variable "web_server_subnets" {
     type = "list"
 }
 variable "terraform_script_version" {}
+variable "domain_name_label" {}
+
+
 
 
 
@@ -61,7 +64,7 @@ resource "azurerm_subnet" "web_server_subnet" {
   resource_group_name       = "${azurerm_resource_group.web_server_rg.name}"
   virtual_network_name      = "${azurerm_virtual_network.web_server_vnet.name}"
   address_prefix            = "${var.web_server_subnets[count.index]}"
-  network_security_group_id = "${azurerm_network_security_group.web_server_nsg.id}"
+  network_security_group_id = "${count.index == 0 ? "${azurerm_network_security_group.web_server_nsg.id}" : ""}"
   count                     = "${length(var.web_server_subnets)}"
 }
 
@@ -87,12 +90,12 @@ resource "azurerm_subnet" "web_server_subnet" {
 #     count                        = "${var.web_server_count}"
 # }
 
-resource "azurerm_public_ip" "web_server_public_ip" {
+resource "azurerm_public_ip" "web_server_lb_public_ip" {
     name                         = "${var.resource_prefix}-public-ip"
     location                     = "${var.web_server_location}"
     resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"
     allocation_method            = "${var.environment == "production" ? "Static" : "Dynamic"}"
-    count                        = "${var.web_server_count}"
+    domain_name_label            = "${var.domain_name_label}"
 }
 
 resource "azurerm_network_security_group" "web_server_nsg" {
@@ -113,7 +116,6 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
     destination_address_prefix  = "*"
     resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
     network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
-    count                       = "${var.environment == "production" ? 1 : 0 }"
 }
 
 resource "azurerm_network_security_rule" "web_server_nsg_rule_ssh" {
@@ -128,9 +130,21 @@ resource "azurerm_network_security_rule" "web_server_nsg_rule_ssh" {
     destination_address_prefix  = "*"
     resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
     network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
-    count                       = "${var.environment == "production" ? 1 : 0 }"
 }
 
+resource "azurerm_network_security_rule" "web_server_nsg_rule_http" {
+    name                        = "RDP Inbound"
+    priority                    = 102
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "TCP"
+    source_port_range           = "*"
+    destination_port_range      = "80"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    resource_group_name         = "${azurerm_resource_group.web_server_rg.name}"
+    network_security_group_name = "${azurerm_network_security_group.web_server_nsg.name}"
+}
 resource "azurerm_virtual_machine_scale_set" "web_server" {
     name                         = "${local.web_server_name}-scale-set"
     location                     = "${var.web_server_location}"
@@ -175,9 +189,10 @@ resource "azurerm_virtual_machine_scale_set" "web_server" {
         primary = true
 
         ip_configuration {
-            name      = "${local.web_server_name}"
-            primary   = true
-            subnet_id = "${azurerm_subnet.web_server_subnet.*.id[0]}"
+            name                                   = "${local.web_server_name}"
+            primary                                = true
+            subnet_id                              = "${azurerm_subnet.web_server_subnet.*.id[0]}"
+            load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id}"]
         }
     }
 
@@ -199,4 +214,44 @@ resource "azurerm_virtual_machine_scale_set" "web_server" {
 #       managed                     = true
 #       platform_fault_domain_count = 2
 # }
+
+resource "azurerm_lb" "web_server_lb" {
+    name                         = "${var.resource_prefix}-lb"
+    location                     = "${var.web_server_location}"
+    resource_group_name          = "${azurerm_resource_group.web_server_rg.name}"  
+
+    frontend_ip_configuration {
+        name                     = "${var.resource_prefix}-lb-frontend-ip"
+        public_ip_address_id     = "${azurerm_public_ip.web_server_lb_public_ip.id}"
+    }
+}
+
+resource "azurerm_lb_backend_address_pool" "web_server_lb_backend_pool" {
+    name                = "${var.resource_prefix}-lb-backend-pool"
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    loadbalancer_id     = "${azurerm_lb.web_server_lb.id}"
+}
+
+resource "azurerm_lb_probe" "web_server_lb_http_probe" {
+    name                = "${var.resource_prefix}-lb-http-probe"
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    loadbalancer_id     = "${azurerm_lb.web_server_lb.id}"
+    protocol            = "tcp"
+    port                = "80"
+}
+
+resource "azurerm_lb_rule" "web_server_lb_http_rule" {
+    name                           = "${var.resource_prefix}-lb-http-probe"
+    resource_group_name            = "${azurerm_resource_group.web_server_rg.name}"
+    loadbalancer_id                = "${azurerm_lb.web_server_lb.id}"
+    protocol                       = "tcp"
+    frontend_port                  = "80"
+    backend_port                   = "80"
+    frontend_ip_configuration_name = "${var.resource_prefix}-lb-frontend-ip"
+    probe_id                       = "${azurerm_lb_probe.web_server_lb_http_probe.id}"
+    backend_address_pool_id        = "${azurerm_lb_backend_address_pool.web_server_lb_backend_pool.id}"
+}
+
+
+
 
