f727c139669ac9c27b66ede6f95f9af11ac8eb74
purplefeetguy <purplefeetguy@hotmail.com>
2020-03-12 09:24:20 -0500

    NSG

--- learning/terraform/main.tf
+++ learning/terraform/main.tf
@@ -19,10 +19,10 @@ resource "azurerm_virtual_network" "web_server_vnet" {
 }
 
 resource "azurerm_subnet" "web_server_subnet" {
-  name                 = "${var.resource_prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.web_server_rg.name
-  virtual_network_name = azurerm_virtual_network.web_server_vnet.name
-  address_prefix       = var.web_server_address_prefix
+  name                      = "${var.resource_prefix}-subnet"
+  resource_group_name       = azurerm_resource_group.web_server_rg.name
+  virtual_network_name      = azurerm_virtual_network.web_server_vnet.name
+  address_prefix            = var.web_server_address_prefix
 }
 
 resource "azurerm_network_interface" "web_server_nic" {
@@ -34,7 +34,7 @@ resource "azurerm_network_interface" "web_server_nic" {
     name                          = "${var.web_server_name}-ip"
     subnet_id                     = azurerm_subnet.web_server_subnet.id
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id = azurerm_public_ip.web_server_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.web_server_public_ip.id
   }
 }
 
@@ -42,5 +42,30 @@ resource "azurerm_public_ip" "web_server_public_ip" {
   name                         = "${var.web_server_name}-public-ip"
   location                     = var.web_server_location
   resource_group_name          = azurerm_resource_group.web_server_rg.name
-  public_ip_address_allocation = "${var.environment == "production" ? "static" : "dynamic"}"
+  allocation_method = var.environment == "production" ? "Static" : "Dynamic"
+}
+
+resource "azurerm_network_security_group" "web_server_nsg" {
+  name                = "${var.web_server_name}-nsg"
+  location            = var.web_server_location
+  resource_group_name = azurerm_resource_group.web_server_rg.name
+}
+
+resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
+  name                        = "RDP Inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "TCP"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.web_server_rg.name
+  network_security_group_name = azurerm_network_security_group.web_server_nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "web_server_subnet_nsg" {
+  subnet_id = azurerm_subnet.web_server_subnet.id
+  network_security_group_id = azurerm_network_security_group.web_server_nsg.id
 }
