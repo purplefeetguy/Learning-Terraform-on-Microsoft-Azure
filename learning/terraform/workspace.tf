6b811bdad2e5f0b5943e6ba61f6c4c1cb19273ac
purplefeetguy <purplefeetguy@hotmail.com>
2020-03-31 06:16:47 -0500

    variables interpolating with workspaces

--- learning/terraform/workspace.tf
+++ learning/terraform/workspace.tf
@@ -1,9 +1,9 @@
 locals {
   web_server_location       = "northcentralus"
-  web_server_rg             = "${terraform.workspace}-web-rg"
-  resource_prefix           = "${terraform.workspace}-web-server"
+  web_server_rg             = "${var.environment}-web-rg"
+  resource_prefix           = "${var.environment}-web-server"
   web_server_address_space  = "10.0.0.0/22"
   web_server_address_prefix = "10.0.1.0/24"
-  web_server_name           = "${terraform.workspace}-web-01"
+  web_server_name           = "${var.environment}-web-01"
   environment               = "${terraform.workspace}"
 }
