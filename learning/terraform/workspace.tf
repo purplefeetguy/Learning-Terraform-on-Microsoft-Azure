54610eb830cdb6e1d943cf2d9587f4b66cd4f42c
purplefeetguy <purplefeetguy@hotmail.com>
2020-04-05 11:39:24 -0500

    continuing to work with workspaces and multiple environments

--- learning/terraform/workspace.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  web_server_location       = "northcentralus"
-  web_server_rg             = "${var.environment}-web-rg"
-  resource_prefix           = "${var.environment}-web-server"
-  web_server_address_space  = "10.0.0.0/22"
-  web_server_address_prefix = "10.0.1.0/24"
-  web_server_name           = "${var.environment}-web-01"
-  environment               = "${terraform.workspace}"
-}
